8cc67f00fa53dea27420fbd6727ae139a52b9f34
Vinh <vinhbuith32412000@gmail.com>
2022-03-24 23:02:32 +0700

    Add post#index, post#new, post#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: :new, :create
+
+  def index 
+    @posts = Post.all 
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:author, :title, :body)
+  end
 end
