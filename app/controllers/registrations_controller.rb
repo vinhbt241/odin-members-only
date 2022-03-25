40db5f9a8714a936b5c353bd4e9e036e3472017c
Vinh <vinhbuith32412000@gmail.com>
2022-03-25 11:46:45 +0700

    Check devise function

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+  def sign_up_params
+    params.require(:member).permit(:icon, :username, :email, :password, :password_confirmation)
+  end
+  def account_update_params
+    params.require(:member).permit(:icon, :username, :email, :password, :password_confirmation, :current_password)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :author, presence: true
-  validates :title, presence: true
-  validates :body, presence: true
+  # validates :author, presence: true
+  # validates :title, presence: true
+  # validates :body, presence: true
 end
